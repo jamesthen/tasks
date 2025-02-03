9e9104f3d74e496a380a3ca28166c438e4576875
jamesthen <18248196+jamesthen@users.noreply.github.com>
2025-02-02 20:16:56 -0500

    updated mytasks static files

--- mytasks/forms.py
+++ mytasks/forms.py
@@ -3,11 +3,11 @@ from django import forms
 
 class AddTaskForm(forms.ModelForm):
     class Meta:
-        model = AddTask  # Link the form to the WikiEntry model
+        model = AddTask  # Link the form to the model
         fields = ['task_name']  # Specify fields to include in the form
         widgets = {
-            'task_name': forms.TextInput(attrs={'rows': 1, 'cols': 1}),  # Add custom widget with attributes
+            'task_name': forms.TextInput(attrs={'rows': 1, 'cols': 1, 'placeholder':'Add a new task'}),  # Add custom widget with attributes
         }
         labels = {
-            'task_name': 'Add Task'  # Custom label for title
+            'task_name': ''  # Custom label for title
         }
