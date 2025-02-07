bc28672ab1d58405e12b73b57a6d943fe70e50e6
James Then <18248196+jamesthen@users.noreply.github.com>
2025-02-07 06:34:18 -0500

    AddGoal route views, forms, and models added

--- mytasks/forms.py
+++ mytasks/forms.py
@@ -1,4 +1,4 @@
-from .models import AddTask
+from .models import AddTask, AddGoal
 from django import forms
 
 class AddTaskForm(forms.ModelForm):
@@ -11,3 +11,8 @@ class AddTaskForm(forms.ModelForm):
         labels = {
             'task_name': ''  # Custom label for title
         }
+
+class AddGoalForm(forms.ModelForm):
+    class Meta:
+        model = AddGoal
+        fields = ['goal_name', 'description', 'due_date', 'completed']
--- mytasks/models.py
+++ mytasks/models.py
@@ -6,3 +6,12 @@ class AddTask(models.Model):
     
     def __str__(self):
         return self.task_name
+    
+class AddGoal(models.Model):
+    goal_name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)  # Larger text
+    due_date = models.DateField()  # Date picker
+    completed = models.BooleanField(default=False)  # Checkbox
+    
+    def __str__(self):
+        return self.goal_name
