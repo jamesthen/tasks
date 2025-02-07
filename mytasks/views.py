a166686b6190a17bf8a7d49b23f4400f702bce99
James Then <18248196+jamesthen@users.noreply.github.com>
2025-02-07 07:21:20 -0500

    views.py included add_items multi-add

--- mytasks/views.py
+++ mytasks/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import AddTaskForm
-from .models import AddTask
+from .forms import AddTaskForm, AddGoalForm
+from .models import AddTask, AddGoal
 
+# def add_items(request):
+#     if request.method == "POST":
+#         task_form = AddTaskForm(request.POST, prefix="task")
+#         goal_form = AddGoalForm(request.POST, prefix="goal")
+
+#         if task_form.is_valid():
+#             task_form.save()
+#         if goal_form.is_valid():
+#             goal_form.save()
+
+#         return redirect("success_url")  # Redirect after submission
+
+#     else:
+#         task_form = AddTaskForm(prefix="task")
+#         goal_form = AddGoalForm(prefix="goal")
+
+#     return render(request, "add_items.html", {"task_form": task_form, "goal_form": goal_form})
 
 def index(request):
     tasks = AddTask.objects.all()
@@ -24,3 +41,15 @@ def remove_task(request, task_id):
         if task_id:
             task_id.delete()
         return redirect('index')
+
+def add_goal(request, goal_id):
+    goals = AddGoal.objects.all()
+    if request.method == "POST":
+        form = AddGoalForm(request.POST)
+    
+
+
+def task_list(request):
+    tasks = Task.objects.all()  # Fetch all tasks
+    task_names = ", ".join(str(task) for task in tasks)  # Calls __str__ on each task
+    return HttpResponse(f"Tasks: {task_names}")
